55f05ab965c12b75c648011596afa570e27b7d88
Charlene Gilbert <Charlene Gilbert>
2016-10-17 18:11:11 -0400

    Assign additional elasticsearch configuration settings to params

--- nefertari/elasticsearch.py
+++ nefertari/elasticsearch.py
@@ -183,12 +183,13 @@ class ES(object):
                     split_strip(each, ':') for each in split_strip(_hosts)]:
                 hosts.append(dict(host=host, port=port))
 
-            params = {}
+            params = dict(cls.settings)
+            del params['hosts']
             if cls.settings.asbool('sniff'):
-                params = dict(
+                params.update(dict(
                     sniff_on_start=True,
                     sniff_on_connection_fail=True
-                )
+                ))
 
             cls.api = elasticsearch.Elasticsearch(
                 hosts=hosts, serializer=engine.ESJSONSerializer(),
